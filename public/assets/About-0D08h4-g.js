dedd2f264d70e99acfff05bd835beedb8036c774
hoang.tran12 <99.hoangtran@gmail.com>
2024-10-29 15:50:42 +0700

    v1.5

---
+++ public/assets/About-0D08h4-g.js
@@ -0,0 +1,1 @@
+import{r as i,av as u}from"./index-CXgZBvTe.js";import{e as s,ac as n}from"./MyApp-D47A_uhM.js";function d({target:o}){return s("About."+(o==null?void 0:o.id),null),i.useEffect(()=>{o!=null&&o.id&&n(o.id)},[o==null?void 0:o.id]),u.jsx(u.Fragment,{children:"About"})}export{d as default};
