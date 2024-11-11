682fcc8305478f435501d9dcc99f9660acf3eb04
hoang.tran12 <99.hoangtran@gmail.com>
2024-11-11 11:49:31 +0700

    fix wait time fetch

--- public/assets/About-BKm-IP6V.js
+++
@@ -1,1 +0,0 @@
-import{r as i,aA as u}from"./index-DKL-YrvT.js";import{g as s,aa as n}from"./MyApp-Y2b_WnTf.js";function l({target:o}){return s("About."+(o==null?void 0:o.id),null),i.useEffect(()=>{o!=null&&o.id&&n(o.id)},[o==null?void 0:o.id]),u.jsx(u.Fragment,{children:"About"})}export{l as default};
--- public/assets/VideoWithMuted-CHVuVQYW.js
+++
@@ -1,1 +0,0 @@
-import{au as u,av as e,aA as r}from"./index-DKL-YrvT.js";function a(t){const o=u(e.muted);return r.jsx("video",{controls:!0,autoPlay:!0,loop:!0,muted:o,...t})}function n(t){const o=u(e.muted);return r.jsx("audio",{controls:!0,autoPlay:!0,loop:!0,muted:o,...t})}export{n as A,a as V};
