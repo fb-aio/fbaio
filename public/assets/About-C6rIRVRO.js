d9ad8458b0191de170773535ce6d124c7465612f
hoang.tran12 <99.hoangtran@gmail.com>
2024-11-18 06:16:44 +0700

    optimize

---
+++ public/assets/About-C6rIRVRO.js
@@ -0,0 +1,1 @@
+import{r as i,aA as u}from"./index-Bz109lEH.js";import{g as s,ad as n}from"./MyApp-Ca9wiXL3.js";function l({target:o}){return s("About."+(o==null?void 0:o.id),null),i.useEffect(()=>{o!=null&&o.id&&n(o.id)},[o==null?void 0:o.id]),u.jsx(u.Fragment,{children:"About"})}export{l as default};
