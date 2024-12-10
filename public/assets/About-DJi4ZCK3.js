37a96a0cc55ed23fea76cd36cb75bd235bfcbb2b
hoang.tran12 <99.hoangtran@gmail.com>
2024-12-10 23:19:28 +0700

    remove coming soon

--- public/assets/About-DJi4ZCK3.js
+++
@@ -1,1 +0,0 @@
-import{r as i,aA as u}from"./index-Dqj9xfKN.js";import{e as s,ac as n}from"./MyApp-CD82law6.js";function d({target:o}){return s("About."+(o==null?void 0:o.id),null),i.useEffect(()=>{o!=null&&o.id&&n(o.id)},[o==null?void 0:o.id]),u.jsx(u.Fragment,{children:"About"})}export{d as default};
