e5af89d1fee3080ec61bc5c39ad18c995e951e31
hoang.tran12 <99.hoangtran@gmail.com>
2024-12-10 23:19:59 +0700

    fix

---
+++ public/assets/About-BwhZv2MS.js
@@ -0,0 +1,1 @@
+import{r as i,aA as u}from"./index-DjNgLHmn.js";import{e as s,ac as n}from"./MyApp-DK69-3iP.js";function d({target:o}){return s("About."+(o==null?void 0:o.id),null),i.useEffect(()=>{o!=null&&o.id&&n(o.id)},[o==null?void 0:o.id]),u.jsx(u.Fragment,{children:"About"})}export{d as default};
